aefdad1ba0cd344a45f6cdf4cbabbcad6a92f3dd
shoukreytom <shoukreytom01@gmail.com>
2020-05-30 15:03:23 +0200

    added icons to logout, update, help and less buttons

--- src/sample/view/control/SidePane.java
+++ src/sample/view/control/SidePane.java
@@ -1,15 +1,17 @@
 package sample.view.control;
 
 import com.jfoenix.controls.JFXButton;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import sample.Main;
 
-public class Sidepane {
+public class SidePane {
 
     @FXML
     private VBox pane;
@@ -20,9 +22,32 @@ public class Sidepane {
     private JFXButton logout = new JFXButton("Log out");
     private JFXButton update = new JFXButton("Update");
     private JFXButton help = new JFXButton("Help");
-    private JFXButton less = new JFXButton("Show less");
+    private JFXButton less = new JFXButton("Less");
 
     public void initialize() {
+        FontAwesomeIconView logoutIcon = new FontAwesomeIconView();
+        FontAwesomeIconView updateIcon = new FontAwesomeIconView();
+        FontAwesomeIconView helpIcon = new FontAwesomeIconView();
+        FontAwesomeIconView lessIcon = new FontAwesomeIconView();
+
+        logoutIcon.setGlyphName("SIGN_OUT");
+        logoutIcon.setGlyphSize(20);
+        logoutIcon.setFill(Color.WHITE);
+        helpIcon.setGlyphName("QUESTION_CIRCLE");
+        helpIcon.setGlyphSize(20);
+        helpIcon.setFill(Color.WHITE);
+        updateIcon.setGlyphName("SLIDERS");
+        updateIcon.setGlyphSize(20);
+        updateIcon.setFill(Color.WHITE);
+        lessIcon.setGlyphName("MINUS_CIRCLE");
+        lessIcon.setGlyphSize(20);
+        lessIcon.setFill(Color.WHITE);
+
+        logout.setGraphic(logoutIcon);
+        help.setGraphic(helpIcon);
+        update.setGraphic(updateIcon);
+        less.setGraphic(lessIcon);
+
         logout.setPrefWidth(200);
         logout.setPrefHeight(35);
         update.setPrefWidth(200);
